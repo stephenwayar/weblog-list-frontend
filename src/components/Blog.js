10442848ed3e8c630eb5f4d30b3282cfbfe81874
stephenwayar <stephenbuluswayar@gmail.com>
2022-10-10 20:01:40 +0100

    added toggle visiblity options to blog details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,27 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [show, setShow] = useState(false)
+
+  const visiblility = { display: show ? '' : 'none' }
+
+  return(
+    <div>
+      <div>
+        <h4>{blog.title}</h4>
+        <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button> 
+      </div>
+
+      <div style={visiblility}>
+        <h5>{blog.author}</h5>
+
+        <h5>{blog.url}</h5>
+
+        <h5>Likes: {blog.likes}</h5>
+        <button>like</button>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
